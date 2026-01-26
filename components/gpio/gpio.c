035efe7b14784c4c10ffb4ce209ba2e839d16583
hoangdesamac <tanthat2019@gmail.com>
2026-01-26 21:02:58 +0700

    init

---
+++ components/gpio/gpio.c
@@ -0,0 +1,8 @@
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
