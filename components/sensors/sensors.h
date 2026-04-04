ed8e025587079120869e8c985755e4fc58d0018a
quangdai <dqdbs06@gmail.com>
2026-04-04 22:39:21 +0700

    full sensor

--- components/sensors/sensors.h
+++ components/sensors/sensors.h
@@ -3,17 +3,15 @@
 
 #include <stdint.h>
 
-// ĐÂY LÀ STRUCT CHUẨN DUY NHẤT SẼ ĐƯỢC DÙNG CẢ Ở NODE VÀ GATEWAY
-// Dùng #pragma pack để tránh lỗi padding memory khi gửi qua ESP-NOW
 #pragma pack(push, 1)
 typedef struct
 {
-    char time_str[16];     // Phục vụ cho OLED (Gateway sẽ nhận nhưng không dùng)
+    char time_str[16];     
     float temperature;
     float humidity;
     float light_level;
     float soil_moisture;
-    uint8_t rain_detected; // Đồng bộ dùng uint8_t với Gateway
+    uint8_t rain_detected; 
 } sensor_data_t;
 #pragma pack(pop)
 
@@ -21,6 +19,6 @@ int sensors_init(void);
 void sensors_read(sensor_data_t *readings);
 int dht11_read(float *temp, float *humidity);
 int adc_read_light(float *level);
-int rain_read(int *detected); // Có thể đổi thành uint8_t* nếu muốn chuẩn hóa hoàn toàn
+int rain_read(int *detected); 
 
 #endif // SENSORS_H
